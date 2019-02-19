a90288e0c31d353c79eed5c5dfce982090808fc0
jfgabriel <jgabriel@neuquen.gov.ar>
2019-02-18 22:35:29 -0300

    fix query publish

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { log } from './src/lib/controller/log';
+import { log, query } from './src/lib/controller/log';
 import { Connections } from './src/lib/controller/initialize';
 
 export {
-    log, Connections
+    log, Connections, query
 };
